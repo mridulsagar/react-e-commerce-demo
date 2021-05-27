ca419fbf9fb9b0584094e31bee301f129ab7c320
mridul-das <mridul.das.work@gmail.com>
2021-05-27 12:20:46 +0600

    + shop page + shop collection-preview + shop collection-item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import Homepage from "./pages/homepage/homepage.component";
 import {Switch, Route} from 'react-router-dom'
-
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-)
+import './App.css'
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
       <div>
           <Switch>
               <Route exact path='/' component={Homepage}/>
-              <Route path='/shop/hats' component={HatsPage}/>
+              <Route path='/shop' component={ShopPage}/>
           </Switch>
       </div>
   );

2cd7cb1927392340cacf5979f987a300e1ae1521
mridul-das <mridul.das.work@gmail.com>
2021-05-31 00:18:15 +0600

    + sign-up.component.jsx + implemented firebase.utils.js with ability to store authenticated user

---
+++ src/test.js
@@ -0,0 +1,7 @@
+import firebase from 'firebase/app'
+
+import 'firebase/firestore'
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('AFb2lGjn7Mdw0JtZG3TJ').collection('cartItems').doc('eJflGSGDmzh7wXoku3F9')
